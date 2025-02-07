ec4611bb45bd2ede75b426ba8d0f2f3ad1cda680
Vinicio Almeida <121837497+vinicioalmeida@users.noreply.github.com>
2025-02-07 15:52:36 -0300

    indent

--- pairstrading.py
+++ pairstrading.py
@@ -1,6 +1,7 @@
 # Pairs Trading S&P
 
 import pandas as pd
+import math
 import numpy as np
 from itertools import combinations
 from statsmodels.regression.linear_model import OLS
@@ -11,7 +12,6 @@ import matplotlib.pyplot as plt
 import datetime
 from datetime import timedelta, time, datetime
 import yfinance as yf
-yf.pdr_override() #to override pandas data fetch with yahoo finance
 import time as tm
 from scipy.optimize import minimize
 from openpyxl import Workbook, load_workbook
@@ -21,16 +21,16 @@ from openpyxl.utils.dataframe import dataframe_to_rows
 end = datetime.now()
 start = end - timedelta(days = 200)
 #from ibov composition file
-assets = ["ABEV3.SA", "ALPA4.SA", "ARZZ3.SA", "ASAI3.SA", "AZUL4.SA", "B3SA3.SA", "BBAS3.SA", 
+assets = ["ABEV3.SA", "ALPA4.SA", "ASAI3.SA", "AZUL4.SA", "B3SA3.SA", "BBAS3.SA", 
         "BBDC3.SA", "BBDC4.SA", "BBSE3.SA", "BEEF3.SA", "BPAC11.SA", "BRAP4.SA", "BRFS3.SA", "BRKM5.SA", 
-        "CASH3.SA", "CCRO3.SA", "CIEL3.SA", "CMIG4.SA", "CMIN3.SA", "COGN3.SA", "CPFE3.SA", "CPLE6.SA", 
+        "CASH3.SA", "CCRO3.SA", "CMIG4.SA", "CMIN3.SA", "COGN3.SA", "CPFE3.SA", "CPLE6.SA", 
         "CRFB3.SA", "CSAN3.SA", "CSNA3.SA", "CVCB3.SA", "CYRE3.SA", "DXCO3.SA", "EGIE3.SA", "ELET3.SA", 
         "ELET6.SA", "EMBR3.SA", "ENEV3.SA", "ENGI11.SA", "EQTL3.SA", "EZTC3.SA", "FLRY3.SA", 
         "GGBR4.SA", "GOAU4.SA", "GOLL4.SA", "HAPV3.SA", "HYPE3.SA", "IGTI11.SA", "IRBR3.SA", "ITSA4.SA", 
         "ITUB4.SA", "JBSS3.SA", "KLBN11.SA", "LREN3.SA", "LWSA3.SA", "MGLU3.SA", "MRFG3.SA", "MRVE3.SA", 
         "MULT3.SA", "NTCO3.SA", "PCAR3.SA", "PETR3.SA", "PETR4.SA", "PETZ3.SA", "PRIO3.SA", "RADL3.SA", 
-        "RAIL3.SA", "RAIZ4.SA", "RDOR3.SA", "RENT3.SA", "RRRP3.SA", "SANB11.SA", "SBSP3.SA", "SLCE3.SA", 
-        "SMTO3.SA", "SOMA3.SA", "SUZB3.SA", "TAEE11.SA", "TIMS3.SA", "TOTS3.SA", "UGPA3.SA", "USIM5.SA", 
+        "RAIL3.SA", "RAIZ4.SA", "RDOR3.SA", "RENT3.SA", "SANB11.SA", "SBSP3.SA", "SLCE3.SA", 
+        "SMTO3.SA", "SUZB3.SA", "TAEE11.SA", "TIMS3.SA", "TOTS3.SA", "UGPA3.SA", "USIM5.SA", 
         "VALE3.SA", "VBBR3.SA", "BHIA3.SA", "VIVT3.SA", "WEGE3.SA", "YDUQ3.SA"]
 quotes = yf.download(assets, start = start, end = end)["Adj Close"]
 quotes.head()
@@ -44,7 +44,7 @@ quotes.columns = [col[:-3] for col in quotes.columns]
 n_days = quotes.shape[0]
 n_assets = quotes.shape[1]
 # Calculate the number of pairs
-num_pairs = np.math.factorial(n_assets) / (np.math.factorial(n_assets - 2) * 2)
+num_pairs = math.factorial(n_assets) / (math.factorial(n_assets - 2) * 2)
 # Create a DataFrame to the pairs
 columns = ["N", "Acao1", "Acao2", "PP1", "PP2", "PPR", "Alfa", "Beta", "pBeta","Preco1", "Preco2", "Max", "Min", "DesvioP", "DesvioAb", "OU"]
 pares = pd.DataFrame(columns=columns)
