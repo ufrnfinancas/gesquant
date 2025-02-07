f93d8eff048c9e2b5b8acde2f6089830d02f7200
Vinicio Almeida <121837497+vinicioalmeida@users.noreply.github.com>
2025-02-07 16:46:26 -0300

    0702

--- pairstrading.py
+++ pairstrading.py
@@ -19,20 +19,21 @@ from openpyxl.utils.dataframe import dataframe_to_rows
 
 # Datas
 end = datetime.now()
-start = end - timedelta(days = 200)
+start = end - timedelta(days = 120)
 #from ibov composition file
 assets = ["ABEV3.SA", "ALPA4.SA", "ASAI3.SA", "AZUL4.SA", "B3SA3.SA", "BBAS3.SA", 
         "BBDC3.SA", "BBDC4.SA", "BBSE3.SA", "BEEF3.SA", "BPAC11.SA", "BRAP4.SA", "BRFS3.SA", "BRKM5.SA", 
         "CASH3.SA", "CCRO3.SA", "CMIG4.SA", "CMIN3.SA", "COGN3.SA", "CPFE3.SA", "CPLE6.SA", 
         "CRFB3.SA", "CSAN3.SA", "CSNA3.SA", "CVCB3.SA", "CYRE3.SA", "DXCO3.SA", "EGIE3.SA", "ELET3.SA", 
-        "ELET6.SA", "EMBR3.SA", "ENEV3.SA", "ENGI11.SA", "EQTL3.SA", "EZTC3.SA", "FLRY3.SA", 
+        "EMBR3.SA", "ENEV3.SA", "ENGI11.SA", "EQTL3.SA", "EZTC3.SA", "FLRY3.SA", 
         "GGBR4.SA", "GOAU4.SA", "GOLL4.SA", "HAPV3.SA", "HYPE3.SA", "IGTI11.SA", "IRBR3.SA", "ITSA4.SA", 
         "ITUB4.SA", "JBSS3.SA", "KLBN11.SA", "LREN3.SA", "LWSA3.SA", "MGLU3.SA", "MRFG3.SA", "MRVE3.SA", 
-        "MULT3.SA", "NTCO3.SA", "PCAR3.SA", "PETR3.SA", "PETR4.SA", "PETZ3.SA", "PRIO3.SA", "RADL3.SA", 
+        "MULT3.SA", "NTCO3.SA", "PCAR3.SA", "PETR4.SA", "PETZ3.SA", "PRIO3.SA", "RADL3.SA", 
         "RAIL3.SA", "RAIZ4.SA", "RDOR3.SA", "RENT3.SA", "SANB11.SA", "SBSP3.SA", "SLCE3.SA", 
         "SMTO3.SA", "SUZB3.SA", "TAEE11.SA", "TIMS3.SA", "TOTS3.SA", "UGPA3.SA", "USIM5.SA", 
         "VALE3.SA", "VBBR3.SA", "BHIA3.SA", "VIVT3.SA", "WEGE3.SA", "YDUQ3.SA"]
-quotes = yf.download(assets, start = start, end = end)["Adj Close"]
+quotes = yf.download(assets, start = start, end = end)
+quotes = quotes["Close"]
 quotes.head()
 quotes.tail()
 #drop a column
@@ -54,7 +55,7 @@ p = n_assets
 # OU functions - # A shorter half-life implies faster mean reversion
 def ornstein_uhlenbeck(params, data):
     theta, mu, sigma = params
-    dt = 1.0 / 252  # Assuming daily data, adjust accordingly
+    dt = 1.0 / 252 
     diff = np.diff(data)
     diff_mu = diff - theta * (mu - data[:-1]) * dt
     sum_squared_diff = np.sum(diff_mu**2)
@@ -132,5 +133,4 @@ df_OU.head(10)
 tenpairs = df_OU.head(10)
 
 # Salve o arquivo Excel
-tenpairs.to_excel('/home/vinicio/repo/harpapro/tenpairs.xlsx', index=False)
-tenpairs.to_excel('/home/vinicio/repo/quant/tenpairs.xlsx', index=False)
+tenpairs.to_excel('C:\\repo\\gesquant\\tenpairs.xlsx', index=False)
